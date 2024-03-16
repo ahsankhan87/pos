51c88a887d84de230c362fd58a28b969f3375e6c
ahsankhan87 <ahsankhan50@gmail.com>
2024-03-16 21:50:02 +0500

    fix

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-    //'port' => 5432,
-	'username' => 'root',//'als',
-	'password' => '',//'acmelogic',
+	//'port' => 5432,
+	'username' => 'root', //'als',
+	'password' => '', //'acmelogic',
 	'database' => 'pos_db',
 	'dbdriver' => 'mysqli', //'sqlsrv',
 	'dbprefix' => '',
